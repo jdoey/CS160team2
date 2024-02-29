3d2c34f17cfdb8e04f7e4bf99880b5aba5e703a5
Jonathan Do <jonathantrido@gmail.com>
2024-02-28 19:21:20 -0800

    test commit

--- api/index.py
+++ api/index.py
@@ -1,5 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
+# from flask_sqlalchemy import SQLAlchemy
+
+# app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://username:password@localhost/db_name'
+
+# db = SQLAlchemy(app)
 
 @app.route("/api/python")
 def hello_world():
