4b98c0de03c6212d3aac68c76803374f4c050248
Danh Nguyen <datvsdanh@gmail.com>
2024-03-21 23:47:38 -0700

    fix db

--- api/index.py
+++ api/index.py
@@ -20,9 +20,11 @@ db_port = os.environ.get("TIDB_PORT")
 db_user = os.environ.get("TIDB_USER")
 db_password = os.environ.get("TIDB_PASSWORD")
 db_database = os.environ.get("TIDB_DATABASE")
+ssl_ca = os.environ.get("SSL_CA")
 
 # Configuring database URI
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_database}?ssl_ca=/etc/ssl/ca-bundle.pem&ssl_verify_cert=true&ssl_verify_identity=true"
+# app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_database}?ssl_ca=/etc/ssl/ca-bundle.pem&ssl_verify_cert=true&ssl_verify_identity=true"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_database}?ssl_ca={ssl_ca}"
  
 # Disable modification tracking
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
