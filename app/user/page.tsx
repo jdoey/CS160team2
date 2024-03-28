f13a9b450929320e12cdfb16e708c39a347afc1c
Victoria Le <95896163+levictoria0117@users.noreply.github.com>
2024-03-28 15:27:46 -0700

    Update page.tsx

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,30 +1,38 @@
 'use client'
+
 import {
     Box,
     Flex,
     Button,
-    Heading
+    Heading,
+    Stack
   } from '@chakra-ui/react'
 
+
 import Sidebar from '../components/Sidebar'
-import AccountCard from '../components/AccountCard'
-import TransactionHistory from '../components/TransactionHistory'
+import AccountCard from './components/AccountCard'
+import TransactionHistory from './components/TransactionHistory'
+import Navbar from '../components/Navbar'
 
 
 export default function Page() {
   return (
     <>
+      <Navbar/>
       <Sidebar/>
       <Box ml={{ base: 0, md: 60 }} p="4">
         <Flex>
-          <Flex flexDirection={'column'}>
+          <Flex flexDirection={'column'} paddingLeft={4} >
+            <Heading paddingBottom={5}>Bank Accounts</Heading>
+            <Stack>
             <AccountCard/>
             <AccountCard/>
-            <Button width='500' alignContent={'center'}>Add Account</Button>
+            </Stack>
+            <Button colorScheme='red'>Add Account</Button>
           </Flex>
-          <Flex flexDirection={'column'}>
-            <Heading paddingTop={2} paddingLeft={8}>Transaction History</Heading>
-            <Flex paddingTop={4} paddingLeft={6}>
+          <Flex paddingLeft={10} flexDirection={'column'}>
+            <Heading paddingBottom={5} >Recent Transaction History</Heading>
+            <Flex>
               <TransactionHistory/>
             </Flex>
           </Flex>
