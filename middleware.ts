05b10eb8b877e344d9ebaf5f613b5aca5d0a3f74
Jonathan Do <jonathantrido@gmail.com>
2024-04-13 01:40:14 -0700

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -6,16 +6,8 @@ export function middleware(request: NextRequest) {
   if (!userToken) {
     return NextResponse.redirect(new URL("/signin", request.url));
   }
-
-  if (
-    userToken &&
-    (request.nextUrl.pathname.startsWith("/signin") ||
-      request.nextUrl.pathname.startsWith("/enroll"))
-  ) {
-    return NextResponse.redirect(new URL("/user", request.url));
-  }
 }
 
 export const config = {
-  matcher: ["/user/:path*", "/signin", "/enroll"],
+  matcher: ["/user/:path*"],
 };
